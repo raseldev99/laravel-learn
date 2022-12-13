0e261964a0a91070a3e2d979a4f57eb9666d4a9d
crazzyboy420 <raselbabu34.bd@gmail.com>
2022-12-13 18:17:06 +0600

    learn

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+         $user = new User();
+         $user->name = 'rasel';
+         $user->email = 'rasel@gmail.com';
+         $user->password = bcrypt("123456");
+         $user->save();
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,4 @@
+<div>
+    <!-- Happiness is not something readymade. It comes from your own actions. - Dalai Lama -->
+    <h1>Alert Template</h1>
+</div>
